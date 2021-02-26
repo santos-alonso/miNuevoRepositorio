7314eedbf2f9d2c52eebf8a5b1d954d1e1bed796
santos-alonso <belen.santos@iesvelazquez.org>
2021-02-26 22:00:41 +0100

    Subiendo proyecto practicaGit

---
+++ Despedida.java
@@ -0,0 +1,8 @@
+package paqueteGitHub;
+
+public class Despedida{
+
+	public static void main(String[] args){
+		System.out.println("Adios chavales, que vaya bien en las empresas");
+	}
+}
---
+++ Saluda.java
@@ -0,0 +1,9 @@
+package paqueteGitHub;
+
+public class Saluda{
+
+	public static void main(String[] args){
+		System.out.println("Hola chavales, ¿cómo estáis?");
+		System.out.println("Estoy mu bien.Soy Juan Antonio López Mera");
+	}
+}
