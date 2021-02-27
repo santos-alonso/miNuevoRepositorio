7f772c180146353ae4d2cc93f7822753e9f4e151
santos-alonso <belen.santos@iesvelazquez.org>
2021-02-27 11:08:44 +0100

    modificando en la nueva rama

--- proyectoGit/Saluda.java
+++ proyectoGit/Saluda.java
@@ -5,5 +5,6 @@ public class Saluda{
 	public static void main(String[] args){
 		System.out.println("Hola chavales, ¿cómo estáis?");
 		System.out.println("Estoy mu bien.Soy Juan Antonio López Mera");
+		System.out.println("Mentira, soy Belén Santos Alonso.");
 	}
 }
