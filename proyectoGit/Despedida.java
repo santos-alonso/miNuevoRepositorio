e51192db544f8c7296e073e7a88e767fdf5b4653
Belén Santos <79721854+santos-alonso@users.noreply.github.com>
2021-02-27 09:30:25 +0100

    Delete Despedida.java

--- proyectoGit/Despedida.java
+++
@@ -1,9 +0,0 @@
-package paqueteGitHub;
-
-public class Despedida{
-
-	public static void main(String[] args){
-		System.out.println("Adios chavales, que vaya bien en las empresas");
-		System.out.println("Gracias :)");
-	}
-}
